394597b4840c7a7fe91956b5c74fd12c93149b19
P-Madhulatha <84167578+P-Madhulatha@users.noreply.github.com>
2021-05-24 09:19:13 +0530

    Add files via upload

---
+++ disarium number.py
@@ -0,0 +1,19 @@
+#disarium number
+
+def isdisarium(num):
+    dc=0
+    temp=num
+    while temp:
+        temp=temp//10
+        dc+=1
+    temp=num
+    res=0
+    while temp:
+        r=temp%10
+        temp//=10
+        res=res+pow(r,dc)
+        dc-=1
+    return res==num
+num=int(input())
+print(isdisarium(num))
+    
