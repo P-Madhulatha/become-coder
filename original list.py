004c085eacf0110837515706bd36a5b5a869235b
P-Madhulatha <84167578+P-Madhulatha@users.noreply.github.com>
2021-06-04 15:03:09 +0530

    Update original list.py

--- original list.py
+++ original list.py
@@ -1,5 +1,5 @@
 #original numbers
-"""def ori(n,data):
+def ori(n,data):
     k=[]
     for i in data:
         if i not in k:
@@ -7,49 +7,13 @@
     return k
 n=int(input())
 data=list(map(int,input().split()))
-print(*ori(n,data))"""
+print(*ori(n,data))
 
 #sorting method
-"""def ori(n,data):
+def ori(n,data):
     data=list(set(data))
     data.sort()
     return data
 n=int(input())
 data=list(map(int,input().split()))
-print(*ori(n,data))"""
-
-#match count
-"""def original(n,data):
-   k=[]
-   c=0
-   for i in data:
-       if i not in k:
-           k.append(i)
-   for i in range(len(k)):
-        if data[i]==k[i]:
-            c+=1
-   return c
-n=int(input())
-data=list(map(int,input().split()))
-c=original(n,data)
-print(c)"""
-
-#match count for sorted list
-def original(n,data):
-    k=[]
-    c=0
-    for i in data:
-        if i not in k:
-            k.append(i)
-    k=list(set(data))
-    for i in range(len(k)):
-        if data[i]==k[i]:
-            c+=1
-    return c
-n=int(input())
-data=list(map(int,input().split()))
-c=original(n,data)
-print(c)
-        
-        
-    
+print(*ori(n,data))
